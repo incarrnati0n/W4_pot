3d5932a2c65fba19116e10155b4cf44d51a7f0e6
incarrnati0n <horvathimi2001@gmail.com>
2023-04-10 16:42:28 +0200

    race seeding removed

--- W4_pot/ViewModels/MainWindowViewModel.cs
+++ W4_pot/ViewModels/MainWindowViewModel.cs
@@ -105,7 +105,7 @@ namespace W4_pot.ViewModels
                 PersonalBest = 130,
                 SeasonalBest = 140,
                 RaceNumber = 7,
-                HasLicense = false,
+                HasLicense = true,
                 HasTeam = true,
             });
 
@@ -119,8 +119,7 @@ namespace W4_pot.ViewModels
                 HasTeam = false,
             });
 
-            Race.Add(Athletes[0].GetCopy());
-            Race.Add(Athletes[1].GetCopy());
+
 
             logic.SetupCollections(Athletes, Race);
 
