e9a87147e7fbb8e1456cb96c5e1517d8929dba3c
incarrnati0n <horvathimi2001@gmail.com>
2023-04-10 16:57:05 +0200

    Save method done

--- W4_pot/MainWindow.xaml.cs
+++ W4_pot/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using W4_pot.Model;
 
 namespace W4_pot
 {
@@ -25,5 +28,21 @@ namespace W4_pot
             InitializeComponent();
         }
 
+        private void Save_click(object sender, RoutedEventArgs e)
+        {
+            List<Athlete> athletes = new List<Athlete>();
+            foreach (var item in lb_race.Items)
+            {
+                athletes.Add(item as Athlete);
+            }
+
+            string JsonData = JsonConvert.SerializeObject(athletes);
+            File.WriteAllText(tb_savename.Text + ".json", JsonData);
+        }
+
+        private void Load_click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
